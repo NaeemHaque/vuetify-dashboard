90b136cc9547635e04cd533a5550f80fe5260e8b
NaemHaque <naeemhaque248@gmail.com>
2021-02-05 17:24:53 +0600

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="home">
+		<v-container>
+			<v-row justify="center" class="mt-3 mb-5 ">
+				<v-text class="headline font-weight-bold">Welcome to our demo dashboard</v-text>
+			</v-row>
+			<v-row class="text-justify">
+				<v-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt laborum dignissimos, sequi reprehenderit ea commodi eaque iure velit fuga aliquam, impedit voluptate sit accusantium qui, totam quae. Omnis, eum error! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt laborum dignissimos, sequi reprehenderit ea commodi eaque iure velit fuga aliquam, impedit voluptate sit accusantium qui, totam quae. Omnis, eum error!</v-text>
+
+				<v-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, alias consectetur natus perferendis totam rem laboriosam facilis ratione nisi, excepturi voluptatem similique. Quae ratione voluptas, impedit enim fugiat eveniet et non, numquam soluta facere perspiciatis. Sint voluptatibus ea labore optio nemo voluptatem eveniet velit! Hic iusto ipsum possimus accusantium voluptatum amet similique minima vel soluta rem ut esse, quidem autem deserunt reiciendis necessitatibus quos aspernatur, in cupiditate consequuntur ducimus neque quaerat. Obcaecati error magni impedit in similique fugit delectus, nam eaque necessitatibus pariatur quos quibusdam dignissimos architecto, nemo sunt molestias accusantium. Sit totam quaerat dicta aperiam eaque debitis eos obcaecati numquam non. Quam eos unde quos eaque. Magnam doloribus libero voluptatibus tenetur exercitationem quam earum at repellendus alias velit maiores inventore facere vel a non id necessitatibus aperiam consequatur vero cumque nemo nostrum, mollitia expedita? Odit, reprehenderit quidem illum provident soluta totam perferendis quasi similique fugiat, dolorum esse aperiam, magnam ducimus alias ex repudiandae libero. Natus ullam amet recusandae modi esse neque aut, corporis deserunt quis quo autem similique optio corrupti ratione aliquid rem nesciunt? Animi beatae ab sint molestiae harum ipsa, eos, cumque, possimus debitis architecto dignissimos distinctio error veniam delectus iste accusantium exercitationem rem et! Quae, voluptate repudiandae!</v-text>
+
+				<v-text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt laborum dignissimos, sequi reprehenderit ea commodi eaque iure velit fuga aliquam, impedit voluptate sit accusantium qui, totam quae. Omnis, eum error! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt laborum dignissimos, sequi reprehenderit ea commodi eaque iure velit fuga aliquam, impedit voluptate sit accusantium qui, totam quae. Omnis, eum error!</v-text>
+			</v-row>
+
+		</v-container>
+		
+		
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+	name: 'home',
+	components: {
+		
+	}
+}
+</script>
